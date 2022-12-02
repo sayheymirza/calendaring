0d5453228b9d2a861b55d5f79c79b84e531ddedc
!Root <iamnonroot@gmail.com>
2022-12-02 16:21:22 +0330

    example updated

--- lib/example.ts
+++ lib/example.ts
@@ -1,8 +1,38 @@
 import { Calendaring } from ".";
 
-
 const calendaring = new Calendaring();
 
-const output = calendaring.Generate(1402, 1)
+const args = process.argv.slice(2);
 
-console.log(output);
+const year = args[0] ? Number(args[0]) : 1401;
+const month = args[1] ? Number(args[1]) : 0;
+const formatter: any =
+  args[2] && ["jalali", "gregorian"].includes(args[2]) ? args[2] : "jalali";
+
+calendaring.SetFormatter(formatter);
+const output = calendaring.Generate(year, month);
+
+console.log(`\t\t${year}/${month}`);
+
+if (formatter == "jalali") {
+  `Sat Sun Mon Tue Wed Thu Fri`
+    .split(" ")
+    .forEach((item) => process.stdout.write(item + "\t"));
+  process.stdout.write("\n");
+}
+
+if (formatter == "gregorian") {
+  `Sun Mon Tue Wed Thu Fri Sat`
+    .split(" ")
+    .forEach((item) => process.stdout.write(item + "\t"));
+  process.stdout.write("\n");
+}
+
+for (let i = 0; i < output.length; i++) {
+  if (i != 0 && i % 7 == 0) process.stdout.write("\n");
+
+  process.stdout.write(
+    (output.array[i].day != 0 ? output.array[i].day : "") + "\t"
+  );
+}
+process.stdout.write("\n");
